ff2ec88276a7d1c732f42d08d6270efb51285ac8
mithunnathan <mithunnathan@gmail.com>
2020-10-23 14:18:26 +0530

    Negative number Test Case

--- src/test/java/com/practice/fizz/FizzTest.java
+++ src/test/java/com/practice/fizz/FizzTest.java
@@ -3,6 +3,7 @@ package com.practice.fizz;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 
 /**
@@ -10,14 +11,27 @@ import org.junit.Test;
  */
 public class FizzTest
 {
+    Fizz fizz;
+    @Before
+    public void Setup()
+    {
+        fizz = new Fizz();
+    }
+
     /**
      * Rigorous Test :-)
      */
     @Test
     public void ifValidNumberIsSent()
     {
-        Fizz fizz = new Fizz();
         int value =fizz.gamePlan(0);
         assertEquals( 0,value );
+    }
+
+    @Test
+    public void ifNegativeNumberIsSetItShouldReturnTheNegativeNumber()
+    {
+        int value =fizz.gamePlan(-2);
+        assertEquals(-2,value);
     }
 }
